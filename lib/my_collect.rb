87d7533a2f1b6f1baed04daa761be534ddeb7c6a
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-09 20:07:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection)
+  i = 0
+  collect = []
+  while i < collection.length
+    collect << yield(collection[i])
+  end
+  collect
+end
